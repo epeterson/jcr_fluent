8697b0ffaf509d9ae1e06f517837eecf8d6ebf8a
Elias Peterson <elias.peterson@gmail.com>
2012-04-15 17:32:35 -0400

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,7 @@
+JCR-Fluent
+
+A fluent query-builder interface for Apache Jackrabbit.
+
+
+Apache Jackrabbit: http://jackrabbit.apache.org/
+Fluent Interface: http://martinfowler.com/bliki/FluentInterface.html
---
+++ src/main/java/com/bitblossom/jcr_fluent/App.java
@@ -0,0 +1,13 @@
+package com.bitblossom.jcr_fluent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
