eb230f1ca2c7ee6747be775e9aff9c6cd2076e22
Elias Peterson <elias.peterson@gmail.com>
2012-04-15 23:21:01 -0400

    Added first files to the project

---
+++ src/main/java/com/bitblossom/jcr_fluent/XPathQueryBuilder.java
@@ -0,0 +1,9 @@
+package com.bitblossom.jcr_fluent;
+
+public class XPathQueryBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/bitblossom/jcr_fluent/XPathQueryBuilderTest.java
@@ -0,0 +1,14 @@
+package com.bitblossom.jcr_fluent;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class XPathQueryBuilderTest {
+
+	@Test
+	public void helloWorldTest() {
+		assertTrue(true);
+	}
+	
+}
